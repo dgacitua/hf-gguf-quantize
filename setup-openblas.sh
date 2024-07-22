cf67b808f5ade1bfed2d453bcb14d391f524acc6
Daniel Gacitúa <daniel.gacitua@usach.cl>
2024-07-21 23:30:14 -0400

    Update setup files to latest llama.cpp

--- setup-openblas.sh
+++ setup-openblas.sh
@@ -3,7 +3,7 @@
 set -euxo pipefail
 
 git clone https://github.com/ggerganov/llama.cpp
-cd llama.cpp && LLAMA_OPENBLAS=1 make && pip install -r requirements.txt
+cd llama.cpp && make GGML_OPENBLAS=1 && pip install -r requirements.txt
 
 pip install "unsloth[colab-new] @ git+https://github.com/unslothai/unsloth.git"
 pip install --no-deps "xformers<0.0.27" "trl<0.9.0" peft accelerate bitsandbytes
