9affb5fc8ee04811637a8a965710d9c2ee3d487a
Daniel Gacitúa <daniel.gacitua@usach.cl>
2024-07-21 23:17:31 -0400

    Add Unsloth to setup script

--- setup-openblas.sh
+++ setup-openblas.sh
@@ -4,3 +4,6 @@ set -euxo pipefail
 
 git clone https://github.com/ggerganov/llama.cpp
 cd llama.cpp && LLAMA_OPENBLAS=1 make && pip install -r requirements.txt
+
+pip install "unsloth[colab-new] @ git+https://github.com/unslothai/unsloth.git"
+pip install --no-deps "xformers<0.0.27" "trl<0.9.0" peft accelerate bitsandbytes
